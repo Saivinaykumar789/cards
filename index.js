32dd36065a08b3d0a9fb54392a48377195fd282a
Saivinaykumar789 <145112206+Saivinaykumar789@users.noreply.github.com>
2024-10-12 19:40:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// let x = document.getElementById("button");
+let x = document.getElementById("button"); // Get the button element
+x.addEventListener("click", function () {
+  // Add click event listener
+  x.innerHTML = "Clicked!";
+});
+// let y = document.getElementsByClassName("card-content");
